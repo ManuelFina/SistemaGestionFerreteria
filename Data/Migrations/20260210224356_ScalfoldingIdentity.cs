95a61f7795b12f1baffd91ec64e00c50196ded23
Manuel Fina <finamanuelmateo@gmail.com>
2026-02-10 19:44:47 -0300

    Migracion con Scaffolding de identity

---
+++ Data/Migrations/20260210224356_ScalfoldingIdentity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaGestionFerreteria.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScalfoldingIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
