191858f4bb8e792bd10cf76e61ce5419f1f8c312
Manuel Fina <finamanuelmateo@gmail.com>
2026-02-10 18:32:17 -0300

    Agregar archivos de proyecto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaGestionFerreteria.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
