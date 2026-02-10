901a5af848a506d8934f924586936e1fb56ded8d
Manuel Fina <finamanuelmateo@gmail.com>
2026-02-10 19:29:55 -0300

    Primera migracion de modelos a Db sqllocal

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SistemaGestionFerreteria.Models;
 
 namespace SistemaGestionFerreteria.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
     }
 }
