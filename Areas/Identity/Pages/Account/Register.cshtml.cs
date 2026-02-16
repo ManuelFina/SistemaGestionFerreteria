6680c4f0759994328cecc06df21e8a1b35c9e4e9
Manuel Fina <finamanuelmateo@gmail.com>
2026-02-16 17:12:17 -0300

    Autenticacion terminada

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using SistemaGestionFerreteria.Models;
 
 namespace SistemaGestionFerreteria.Areas.Identity.Pages.Account
 {
@@ -29,13 +30,15 @@ namespace SistemaGestionFerreteria.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -43,6 +46,8 @@ namespace SistemaGestionFerreteria.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _roleManager = roleManager;
+            
         }
 
         /// <summary>
@@ -119,30 +124,29 @@ namespace SistemaGestionFerreteria.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
-                {
-                    _logger.LogInformation("User created a new account with password.");
-
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                { 
+                    if(!await _roleManager.RoleExistsAsync(Roles.Administrador))
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                     await _roleManager.CreateAsync(new IdentityRole(Roles.Administrador));
+                     await _roleManager.CreateAsync(new IdentityRole(Roles.Registrado));
+                     await _roleManager.CreateAsync(new IdentityRole(Roles.Empleado));
                     }
-                    else
+                string rol = Request.Form["radUsuarioRole"].ToString();
+                    
+                    if (rol == Roles.Administrador)
+                    { 
+                        await _userManager.AddToRoleAsync(user, Roles.Administrador);
+                    }
+                    else if (rol == Roles.Registrado)
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
+                        await _userManager.AddToRoleAsync(user, Roles.Registrado);
                     }
+                    else if (rol == Roles.Empleado)
+                    {
+                        await _userManager.AddToRoleAsync(user, Roles.Empleado);
+                    }
+
+
                 }
                 foreach (var error in result.Errors)
                 {
---
+++ Models/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaGestionFerreteria.Models
+{
+    public class Roles
+    {
+        public const string Administrador = "Administrador";
+        public const string Registrado = "Registrado";
+        public const string Empleado = "Empleado";
+
+    }
+}
