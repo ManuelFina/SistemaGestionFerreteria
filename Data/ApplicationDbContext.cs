24b257c9735675c7ebc5ce8b326d11396f1adb72
Manuel Fina <finamanuelmateo@gmail.com>
2026-05-18 18:42:33 -0300

    feat(configuracion): configurar entity framework e identity
    - Se configuro ApplicationDbContext
    - Se agregaron DbSet para las entidades
    - Se configuro SQL Server
    - Se configuro ASP.NET Core Identity
    - Se habilito autenticacion y autorizacion
    - Se creo la migracion inicial

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SistemaGestionFerreteria.Models.Entities;
 
 namespace SistemaGestionFerreteria.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        public DbSet<Proveedor> Proveedores { get; set; }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Venta> Ventas { get; set; }
+
+        public DbSet<DetalleVenta> DetalleVentas { get; set; }
     }
+
+
 }
